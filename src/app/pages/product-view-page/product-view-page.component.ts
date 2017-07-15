1adf176621d6cc50b2fb0162ecbf6f5c254c98ad
Alvin James Bellero <ajames.bellero@gmail.com>
2017-07-15 17:47:54 +0800

    Created product component page

---
+++ src/app/pages/product-view-page/product-view-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-view-page',
+  templateUrl: './product-view-page.component.html',
+  styleUrls: ['./product-view-page.component.css']
+})
+export class ProductViewPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
